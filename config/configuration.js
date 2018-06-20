8ac82207814fbe060a29828be66d90f3e6f94aac
AdnanShah <adnanshah9911@gmail.com>
2018-06-19 18:23:06 -0700

    nodemailer api end

---
+++ config/configuration.js
@@ -0,0 +1,29 @@
+/**
+ * Created with Visual Studio Code.
+ * User: Abdul jabbar
+ * Date: 20/07/2017
+ * Time: 10:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+//mysql://b63473378d9395:bdfe2d82@us-cdbr-iron-east-05.cleardb.net/heroku_edbf8b0abae80ee?reconnect=true
+
+var serverURL = 'https://lmutest.herokuapp.com/';
+var secret = 'hT_$øper$ecreT';
+var host = 'localhost';
+var user = 'root';
+var password = null;
+//var password = 'hyj6h5H6!5Tr';
+var database = 'quickserver';
+var accessKey = "ADBB-6CA3-15AE-359E";
+const client_Id = "AYVt91d6oWbKRxsqhxarOVxLJjPbznMAZ-xAR7faS65XgmdtN3KH-wj8fceN3X8Oe61WWbtVza2rz3N2";
+const client_secret="EDfo11DyRCYE6TMLkayK4y5qXXn-Yrol41c6UD2rXyJZALhQsZaQqh1U27RQVm-4kVwglvsqQ7y5q_ML";
+module.exports.secret = secret;
+module.exports.host = host;
+module.exports.user = user;
+module.exports.password = password;
+module.exports.database = database;
+module.exports.serverURL = serverURL;
+module.exports.accessKey = accessKey;
+module.exports.client_Id=client_Id;
+module.exports.client_secret=client_secret;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,104 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var mysql = require("../config/database.js");
+
+var express = require("express");
+var router = express.Router();
+// var mysql = require("../config");
+var async = require("async");
+const Promise = require("bluebird");
+const nodemailer = require("nodemailer");
+
+router.post("/register", (req, res) => {
+  req.checkBody("name").notEmpty();
+  req.checkBody("email").notEmpty();
+  req.checkBody("number").notEmpty();
+  req.checkBody("reason").notEmpty();
+  req.checkBody("details").notEmpty();
+  req.getValidationResult().then(error => {
+    if (!error.isEmpty()) {
+      res.json({
+        status: 403,
+        message: "ServerMandatoryParameterMissing",
+        error: error
+      });
+    } else {
+      let data = {
+        name: req.body.name,
+        number: req.body.number,
+        reason: req.body.reason,
+        details: req.body.details,
+        email: req.body.email
+      };
+      // if (req.body.category_id == "" || req.body.category_id == null) {
+      //   data.category_id = null;
+      // } else {
+      //   data.category_id = req.body.category_id;
+      // }
+      // const saltRounds = 10;
+      // //https://www.npmjs.com/package/bcrypt
+      // bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+      // data.password = hash;
+      // console.log(data.password);
+      const selectQry =
+        "select email from users" + ' where email = "' + req.body.email + '" ';
+      const insertQry = `insert into users set ?`;
+      Promise.using(mysql.getSqlConn(), conn => {
+        conn
+          .query(selectQry)
+          .then(rows => {
+            if (rows.length > 0) {
+              res.json({ status: 403, message: "EmailAlreadyExists" });
+            } else {
+              conn.query(insertQry, data).then(user => {
+                var smtpTrans = nodemailer.createTransport({
+                  service: "Gmail",
+                  auth: {
+                    user: "adnanshah9911@gmail.com",
+                    pass: "52193610shah"
+                  }
+                });
+                var mailOptions = {
+                  to: req.body.email,
+                  from: "lookmeuplmu@gmail.com",
+                  subject: "Look Me Up Activation Code",
+                  text:
+                    "Welcome to LOOK ME UP !!! Glad to see You here.\n\n" +
+                    "Please enter on the following code, or paste this into your app to complete the process:\n\n" +
+                    "your code is => " +
+                    JSON.stringify(data) +
+                    "\n\n"
+                };
+                smtpTrans
+                  .sendMail(mailOptions)
+                  .then(() => {
+                    res.json({
+                      status: 200,
+                      message: "User Registered And Email Sent Successfully"
+                    });
+                  })
+                  .catch(err => {
+                    res.json({
+                      status: 401,
+                      message: "Email not sent",
+                      Error: err
+                    });
+                  });
+              });
+            }
+          })
+          .catch(err => {
+            res.json({ status: 500, error: "QueryError" + err });
+          });
+      });
+      // }); // hash
+    }
+  }); // getValidationResult
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
 });
 
 module.exports = router;
